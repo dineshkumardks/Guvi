2900a077f2f64027418bfd6569e931903736b717
Dinesh Kumar S <46812347+dineshkumardks@users.noreply.github.com>
2019-01-31 22:33:42 +0530

    Create largenum.c

---
+++ largenum.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int m,n,o;
+scanf("%d%d%d",&m,&n,&o);
+if((m>n)&&(m>n))
+printf("m is greater");
+else if((n>m)&&(n>o))
+printf("n is greater");
+else
+printf("o is greater");
+}
