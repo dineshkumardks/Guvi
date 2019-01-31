434e8c147441b9b6d0e86e309aafcc249d91b92d
Dinesh Kumar S <46812347+dineshkumardks@users.noreply.github.com>
2019-01-31 21:56:29 +0530

    Update oddeve.py

--- oddeve.py
+++ oddeve.py
@@ -1,5 +1,7 @@
 b=int(input("Enter an integer:"))
 if ((b%2)==0):
     print("the number is even")
-else:
-print("the number is odd")
+elif (b<0):
+    print <"the number is invalid")
+ else :   
+    print("the number is odd")
