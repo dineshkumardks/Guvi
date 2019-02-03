63a6bbf6c3b6a4692394e4a150a0f5d84a273f99
Dinesh Kumar S <46812347+dineshkumardks@users.noreply.github.com>
2019-02-03 19:46:23 +0530

    Create primeintervalcheck.c

---
+++ primeintervalcheck.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int j,k,l,m;
+scanf("%d %d",&j,&k);
+for(l=j;l<k;l++)
+{
+	int count=0;
+	for(m=2;m<l;m++)
+	{
+	if(l%m==0)
+	count++;
+	}
+	if(count==0)
+	printf("%d\t",l);
+}
+return 0;
+}
