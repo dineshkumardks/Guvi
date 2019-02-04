00ab2f331f96fbf5de548d2e46fcc13afaadae45
Dinesh Kumar S <46812347+dineshkumardks@users.noreply.github.com>
2019-02-04 11:15:09 +0530

    Create armstrngintervals.py

---
+++ armstrngintervals.py
@@ -0,0 +1,17 @@
+a,b,=map(int,input().split())
+for i in range(a,b):
+ y=i
+ t=0
+ u=0
+ while(y>0):
+    c=y%10
+    c=c*c*c
+    u=u+c
+    y=int(y/10)
+ if(u==i):
+   if(t==1):  
+        print("  ")
+        print(u,end=" ")
+   else:
+        print(u,end=" ")
+        t=1
