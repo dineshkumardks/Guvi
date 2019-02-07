184494204ea43ce63249b5f411965fae23ad2f37
Dinesh Kumar S <46812347+dineshkumardks@users.noreply.github.com>
2019-02-07 20:14:36 +0530

    Update playervowels.py

--- playervowels.py
+++ playervowels.py
@@ -1,7 +1,7 @@
 b=int(input())
 v=list(input())
 h=[]
-for i in range(0,b+1):
+for i in range(0,len(v)):
     if(v[i]!='a' and v[i]!='e' and v[i]!='i' and v[i]!='o' and v[i]!='u' and v[i]!='A' and v[i]!='E' and v[i]!='I' and v[i]!='O' and v[i]!='U'):
         h.append(v[i])
 h.reverse()
