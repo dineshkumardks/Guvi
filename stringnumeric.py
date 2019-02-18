53763cdaa8d354d01572eeb2c7a375af2391a82e
Dinesh Kumar S <46812347+dineshkumardks@users.noreply.github.com>
2019-02-18 22:42:10 +0530

    Create stringnumeric.py

---
+++ stringnumeric.py
@@ -0,0 +1,11 @@
+str=input()
+num=".0123456789"
+flag=0
+for i in range(0,len(str)):
+	      if str[i] not in num:
+		         flag=1
+		         break
+if flag==0:
+	  print("Yes")
+else:
+    print("No")
