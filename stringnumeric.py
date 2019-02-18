da53153665728932de68283c3f97a66c2eb2f2a0
Dinesh Kumar S <46812347+dineshkumardks@users.noreply.github.com>
2019-02-18 22:43:19 +0530

    Update stringnumeric.py

--- stringnumeric.py
+++ stringnumeric.py
@@ -2,10 +2,10 @@ str=input()
 num=".0123456789"
 flag=0
 for i in range(0,len(str)):
-	      if str[i] not in num:
-		         flag=1
-		         break
+	if str[i] not in num:
+		flag=1
+		break
 if flag==0:
-	  print("Yes")
+	print("Yes")
 else:
-    print("No")
+    	print("No")
