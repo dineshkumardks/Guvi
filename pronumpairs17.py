16cd7c6caf682dc10e15804cd741029e6c6a67c7
Dinesh Kumar S <46812347+dineshkumardks@users.noreply.github.com>
2019-02-18 22:35:24 +0530

    Create pronumpairs17.py

---
+++ pronumpairs17.py
@@ -0,0 +1,13 @@
+n,k=map(int,input().split())
+a=0
+s=[int(x) for x in input().split()]
+for i in range(0,n):
+    for j in range(0,n):
+        if(i!=j):
+            if(s[i]+s[j]==k):
+                a=1
+                break
+if(a==0):
+    print("no")
+else:
+print("yes")
