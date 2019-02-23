2ea97dae3eae1fd9abb2c22fcd7f2b39559a57d0
Dinesh Kumar S <46812347+dineshkumardks@users.noreply.github.com>
2019-02-23 19:46:51 +0530

    Update pro253increasingsubarray.py

--- pro253increasingsubarray.py
+++ pro253increasingsubarray.py
@@ -5,11 +5,11 @@ c=1
 for i in range(n-1):
 	if  inc[i]<inc[i+1]:
                   c+=1
-		  if i==n-2:
-			if c>l:
-			      l=c
+		   if i==n-2:
+			   if c>l:
+			        l=c
 	 else:
 	         if c>l:
 	              l=c
-         c=1
+                 c=1
 print(l)
