dfaf450efc90932df28fdaa2dc9cba90b2815f15
Dinesh Kumar S <46812347+dineshkumardks@users.noreply.github.com>
2019-02-23 19:44:18 +0530

    Update pro253increasingsubarray.py

--- pro253increasingsubarray.py
+++ pro253increasingsubarray.py
@@ -3,13 +3,13 @@ inc=list(map(int,input().split()))
 l=0
 c=1
 for i in range(n-1):
-	      if  inc[i]<inc[i+1]:
+	if  inc[i]<inc[i+1]:
                   c+=1
-		              if i==n-2:
-			                    if c>l:
-				                       l=c
-				else:
-		          if c>l:
-			             l=c
-		          c=1
+		  if i==n-2:
+			if c>l:
+			      l=c
+	 else:
+	         if c>l:
+	              l=c
+         c=1
 print(l)
