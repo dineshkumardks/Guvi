71e4c87e59b37615bb55e2b36dc20c0075ad394c
Dinesh Kumar S <46812347+dineshkumardks@users.noreply.github.com>
2019-02-23 21:45:00 +0530

    Update substringpro657.py

--- substringpro657.py
+++ substringpro657.py
@@ -1,11 +1,11 @@
-s1,s2=input().split()
+a,b=input().split()
 i=0
 tmp=0
-while i+2<=len(s1):
-	if s1[i:i+2] in s2:
-	     tmp=1
-             print("yes")
-             break
+while i+2<=len(a):
+	if a[i:i+2] in b:
+	      tmp=1
+              print("yes")
+              break
 	i+=1
 if tmp==0:
-      print("no")
+       print("no")
