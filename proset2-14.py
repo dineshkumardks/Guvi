746279552683f23ccd2a5d1263df1c875e7c4de6
Dinesh Kumar S <46812347+dineshkumardks@users.noreply.github.com>
2019-06-13 21:48:38 +0530

    Create proset2-14.py

---
+++ proset2-14.py
@@ -0,0 +1,13 @@
+#dineshdks
+a,b=map(int,input().split())
+m=list(map(int,input().split()))
+tq=[]
+for i in range(0,b):
+    d = []
+    d=list(map(int,input().split()))
+    s = m[d[0]-1]
+    for j in range(min(d),max(d)):
+        s=s^m[j]
+    tq.append(s)
+for i in range(0,len(tq)):
+print(tq[i])
